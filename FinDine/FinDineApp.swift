418626f2e70141b687147be920f5cede0665b469
eason <fengguoyi1223@gmail.com>
2022-08-18 20:42:16 -0700

    Initial Commit

---
+++ FinDine/FinDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinDineApp.swift
+//  FinDine
+//
+//  Created by Eason Feng on 2022-08-18.
+//
+
+import SwiftUI
+
+@main
+struct FinDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
