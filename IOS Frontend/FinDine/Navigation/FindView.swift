24b38bad28abb9ae09e70b982d5a5bfcb94ba1c7
Peter <Na>
2022-08-21 00:09:42 -0700

    pull from remote

---
+++ IOS Frontend/FinDine/Navigation/FindView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  FinDine
+//
+//  Created by Peter Na on 2022-08-20.
+//
+
+import SwiftUI
+
+struct FindView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FindView_Previews: PreviewProvider {
+    static var previews: some View {
+        FindView()
+    }
+}
